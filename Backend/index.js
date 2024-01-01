347e8fd30a37915a8461ba4d4c9063a347880864
sinvan <muhammedsinvan3@gmail.com>
2024-01-01 16:10:08 +0530

    router setted

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,20 @@
 import express from 'express' 
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
 import { connectDB } from './Connection/database.js';
+import userRouter from './Routes/cliend.js';
 
 connectDB();
 
 const app = express();
 
 app.use(express.json({limit:'25mb'})); 
-app.use(express.urlencoded({limit:'25mb',extended:true}))
+app.use(express.urlencoded({limit:'25mb',extended:true}));
+app.use(cors());
 
+
+app.use('/api/',userRouter);
 
 
 app.listen(8000,()=>{
